5d4b521889dbb8ee3d2c208512c1de18316ea6ac
Mohammed Hamza Jasnak <mh342039@dal.ca>
2021-02-25 12:25:01 +0530

    update

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Login {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter");
+        int y = sc.nextInt();
     }
     public  int test (){
         return  10;
