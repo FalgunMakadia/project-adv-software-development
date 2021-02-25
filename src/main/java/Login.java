a5d37d0231888219fc5701b654793027b7fcf984
Mohammed Hamza Jasnak <mh342039@dal.ca>
2021-02-25 18:00:47 +0530

    w

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Login {
 
     public static void main(String[] args) {
-        System.out.println("Hello world 1");
+        System.out.println("Login Page");
         Scanner sc = new Scanner(System.in);
         System.out.println("enter");
         int y = sc.nextInt();
