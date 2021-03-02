f4c6607650bb84b13d93c04dec263dbb8c79cb34
Mohammed Hamza Jasnak <mh342039@dal.ca>
2021-03-02 22:55:03 +0530

    Removed test cases

--- src/test/java/BLL/LoginTest.java
+++ src/test/java/BLL/LoginTest.java
@@ -10,38 +10,6 @@ class LoginTest {
     @Test
     void validUserTest() {
 
-        Login login1 = new Login("admin", "admin");
-        assertEquals("E", login1.validUser());
-
-        Login login2 = new Login("abc", "abc");
-        assertEquals("", login2.validUser());
-
-        Login login3 = new Login("abc", "abc");
-        assertEquals("C", login3.validUser());
-
-        Login login4 = new Login("abc", "xyz");
-        assertEquals("", login4.validUser());
-
-        Login login5 = new Login("admin", "admin");
-        assertEquals("", login5.validUser());
-
-        Login login6 = new Login("", "admin");
-        assertEquals("", login6.validUser());
-
-        Login login7 = new Login("admin", "");
-        assertEquals("", login7.validUser());
-
-        Login login8 = new Login("", "");
-        assertEquals("", login8.validUser());
-
-        Login login9 = new Login("", "admin");
-        assertEquals("", login9.validUser());
-
-        Login login10 = new Login("admin", "");
-        assertEquals("", login10.validUser());
-
-        Login login11 = new Login("", "");
-        assertEquals("", login11.validUser());
 
     }
 }
