825b6d22666c35105e54a40666b95fa1525face9
Mohammed Hamza Jasnak <mh342039@dal.ca>
2021-03-19 16:22:26 +0530

    updated the test cases for the loan estimator

--- src/test/java/BusinessLogicLayer/CustomerAction/LoanEstimatorTest.java
+++ src/test/java/BusinessLogicLayer/CustomerAction/LoanEstimatorTest.java
@@ -8,6 +8,30 @@ public class LoanEstimatorTest {
     public void calculateEmiTest(){
         LoanEstimator loanEstimator = new LoanEstimator();
 
-        assertEquals(10179.160528647808  ,loanEstimator.calculateEmi(200000,2));
+        assertEquals(45.22729145618458  ,loanEstimator.calculateEmi(1000,2));
+    }
+    @Test
+    public void calculateEmiFirstBracketTest(){
+        LoanEstimator loanEstimator = new LoanEstimator();
+
+        assertEquals(452.2729145618458  ,loanEstimator.calculateEmi(10000,2));
+    }
+    @Test
+    public void calculateEmiSecondBracketTest(){
+        LoanEstimator loanEstimator = new LoanEstimator();
+
+        assertEquals(1130.6822864046146  ,loanEstimator.calculateEmi(25000,2));
+    }
+    @Test
+    public void calculateEmiThirdBracketTest(){
+        LoanEstimator loanEstimator = new LoanEstimator();
+
+        assertEquals(2487.501030090152  ,loanEstimator.calculateEmi(55000,2));
+    }
+    @Test
+    public void calculateEmiFourthBracketTest(){
+        LoanEstimator loanEstimator = new LoanEstimator();
+
+        assertEquals(9045.458291236917  ,loanEstimator.calculateEmi(200000,2));
     }
 }
