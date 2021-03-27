399c27872196d2ae030cd7430b56dcbb44df46d0
Preetham Tikkavarapu <pr836619@dal.ca>
2021-03-27 16:29:33 -0300

    Revert "Merge branch 'UpdatePassword' into 'Develop'"
    This reverts merge request !40

--- src/main/java/BusinessLogicLayer/CustomerAction/UpdatePassword.java
+++ src/main/java/BusinessLogicLayer/CustomerAction/UpdatePassword.java
@@ -1,29 +1,9 @@
 package BusinessLogicLayer.CustomerAction;
 
-import DataAccessLayer.DatabaseFactory;
-import DataAccessLayer.IDatabaseFactory;
-import DataAccessLayer.IUserDetailsDatabase;
 import BusinessLogicLayer.CommonAction.Action;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 public class UpdatePassword extends Action {
-    UpdatePassword updatePassword;
-    String newpassword;
-
-    public String getNewpassword() {
-        return newpassword;
-    }
-
-    public void setNewpassword(String newpassword) {
-        this.newpassword = newpassword;
-    }
-
-
     private static final String menuLabel = "Update Password";
-    IUserDetailsDatabase userDetailsDatabase = null;
-
 
     @Override
     public String getMenuLabel() {
@@ -31,34 +11,13 @@ public class UpdatePassword extends Action {
     }
 
     @Override
-    protected void setCurrentPageInContext() {
-        loggedInUserContext.setCurrentPage(menuLabel);
-    }
-
-    @Override
     public void performAction() {
         setCurrentPageInContext();
         userInterface.displayMessage("Update Password");
-        String userName = loggedInUserContext.getUserName();
-        try {
-            IDatabaseFactory databaseFactory = new DatabaseFactory();
-            IUserDetailsDatabase userDetailsDatabase = databaseFactory.createLoginDatabase();
-            userInterface.displayMessage("Hello " + userName);
-            String newPassword = userInterface.getMandatoryUserInput("New Password*: ");
-            String ConfirmPassword = userInterface.getMandatoryUserInput("Confirm New Password*: ");
-            if (newPassword.equals(ConfirmPassword))
-                newPassword.hashCode();
-            updatePassword.setNewpassword(newPassword);
-            userInterface.displayMessage(userName + " your new  password is updated: ");
-            userInterface.insertEmptyLine();
-            userInterface.insertEmptyLine();
-            userDetailsDatabase.UpdatePassword(userName, newPassword);
-        } catch (IOException throwables) {
-            throwables.printStackTrace();
+    }
 
-
-        }
-
-
+    @Override
+    protected void setCurrentPageInContext() {
+        loggedInUserContext.setCurrentPage(menuLabel);
     }
 }
--- src/main/java/DataAccessLayer/IUserDetailsDatabase.java
+++ src/main/java/DataAccessLayer/IUserDetailsDatabase.java
@@ -6,6 +6,4 @@ public interface IUserDetailsDatabase {
     public ResultSet validateUser(String username, int password);
 
     public int insertNewUser();
-
-    void UpdatePassword(String userName, String newPassword);
 }
