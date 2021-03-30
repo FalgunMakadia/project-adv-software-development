93cb94f62dd39a333727a0bf1362776fddeceac7
Nikunj Dhola <nk309459@dal.ca>
2021-03-30 14:04:53 +0530

    displaying worklist

--- src/main/java/DataAccessLayer/IWorklistDatabase.java
+++ src/main/java/DataAccessLayer/IWorklistDatabase.java
@@ -3,9 +3,12 @@ package DataAccessLayer;
 import BusinessLogicLayer.WorklistRequest.WorklistRequest;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface IWorklistDatabase {
     int addWorkListRequest(WorklistRequest worklistRequest) throws SQLException;
 
     WorklistRequest getWorkListRequest(int id);
+
+    ArrayList<WorklistRequest> getWorkLists();
 }
