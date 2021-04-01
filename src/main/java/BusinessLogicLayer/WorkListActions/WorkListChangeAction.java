df10bf7a9ad3e31bee1a5a075820c54b66a7bbbc
Nikunj Dhola <nk309459@dal.ca>
2021-04-01 09:04:28 +0530

    Worklist for new account is done

--- src/main/java/BusinessLogicLayer/WorkListActions/WorkListChangeAction.java
+++ src/main/java/BusinessLogicLayer/WorkListActions/WorkListChangeAction.java
@@ -7,38 +7,32 @@ import DataAccessLayer.ICustomerDatabase;
 import java.sql.SQLException;
 
 public class WorkListChangeAction extends WorkListAction {
-    ICustomerDatabase customerDatabase;
 
     public WorkListChangeAction(WorklistRequest worklistRequest, int worklistID) {
         super(worklistRequest, worklistID);
-        customerDatabase = databaseFactory.createCustomerDatabase();
     }
 
     @Override
     public void processWorkList() {
         showWorkListDetail();
 
-        String userInput = userInterface.getConfirmation("Assign to me ?");
-        if (userInput.equalsIgnoreCase("y")) {
-            String empUserName = loggedInUserContext.getUserName();
-            boolean isUpdated = worklistDatabase.updateAssignee(worklistID, empUserName);
-            if (isUpdated) {
-                showComparisonOfUserDetails();
+        boolean isAssigned = assignWorklist();
+        if (isAssigned) {
+            showComparisonOfUserDetails();
 
-                String processInput = userInterface.getConfirmation("Do you want to process this change");
-                if (processInput.equalsIgnoreCase("y")) {
-                    boolean isDetailsUpdated = customerDatabase.
-                            updateUser(worklistRequest.getAccountNumber(), worklistRequest.getUser());
+            String processInput = userInterface.getConfirmation("Do you want to process this change");
+            if (processInput.equalsIgnoreCase("y")) {
+                boolean isDetailsUpdated = customerDatabase.
+                        updateUser(worklistRequest.getAccountNumber(), worklistRequest.getUser());
 
-                    if (isDetailsUpdated) {
-                        userInterface.displayMessage("User details updated successfully");
-                    } else {
-                        userInterface.displayMessage("Error in updating user details");
-                    }
+                if (isDetailsUpdated) {
+                    userInterface.displayMessage("User details updated successfully");
+                } else {
+                    userInterface.displayMessage("Error in updating user details");
                 }
-            } else {
-                userInterface.displayMessage("Error in updating work list details...");
             }
+        } else {
+            userInterface.displayMessage("Error in updating work list details...");
         }
     }
 
