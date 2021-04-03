b70ffa682a8dc1d0e29eb6af6bc5db7caeb556e3
Preetham Tikkavarapu <pr836619@dal.ca>
2021-04-03 05:24:58 -0300

    Update IUserDetailsDatabase

--- src/main/java/DataAccessLayer/IUserDetailsDatabase.java
+++ src/main/java/DataAccessLayer/IUserDetailsDatabase.java
@@ -6,4 +6,8 @@ public interface IUserDetailsDatabase {
     public ResultSet validateUser(String username, int password);
 
     public int insertNewUser();
+
+    void UpdatePassword(String userName, int changedPassword);
+
+    void insertNewEmployee(String userName,int defaultPassword, String userRole);
 }
